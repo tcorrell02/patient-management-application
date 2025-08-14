b426f24e2d3fbe592c9cbb5f270cfa538d66eb22
tcorrell02 <tyler.correll02@gmail.com>
2025-08-14 11:11:06 -0400

    DTO for handling communication between service and controller, Mapper for easier DTO conversion

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,36 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        /* Use method reference for cleaner code
+        List<PatientResponseDTO> patientResponseDTOs = patients.stream()
+                .map(PatientMapper::toDTO).toList();
+
+        */
+        /*
+        Can also use a lambda function like this:
+        List<PatientResponseDTO> patientResponseDTOs = patients.stream()
+                .map(patient -> PatientMapper.toDTO(patient)).toList();
+
+        */
+        // Use Inline variable with method reference for simplicity
+        return patients.stream().map(PatientMapper::toDTO).toList();
+    }
+}
